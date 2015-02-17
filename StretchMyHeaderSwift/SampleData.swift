8806c75ccdff94efa3cf824afb4415a213fa6614
Mike Reining <emailmike@gmail.com>
2015-02-17 11:10:01 -0800

    Project Setup

---
+++ StretchMyHeaderSwift/SampleData.swift
@@ -0,0 +1,20 @@
+//
+//  SampleData.swift
+//  StretchMyHeader
+//
+//  Created by Michael Reining on 2/17/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import Foundation
+
+var data = [
+    ["World": "Climate change protests, divestments meet fossil fuels realities"],
+    ["Europe": "Scotland's 'Yes' leader says independence vote is 'once in a lifetime'"],
+    ["Middle East": "Airstrikes boost Islamic State, FBI director warns more hostages possible"],
+    ["Africa": "Nigeria says 70 dead in building collapse; questions S. Africa victim claim"],
+    ["Asia Pacific":"Despite UN ruling, Japan seeks backing for whale hunting"],
+    ["Americas":"Officials: FBI is tracking 100 Americans who fought alongside IS in Syria"],
+    ["World":"South Africa in $40 billion deal for Russian nuclear reactors"],
+    ["Europe":"One million babies' created by EU student exchanges"]
+]
